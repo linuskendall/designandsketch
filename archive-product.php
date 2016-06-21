29858f69a2fff3e3246c80621e314fe459df1ff8
Linus Kendall <git@linuskendall.com>
2016-06-21 21:14:42 +0530

    Added placeholder image

--- archive-product.php
+++ archive-product.php
@@ -1,16 +1,17 @@
 <?php global $no_sidebar; $no_sidebar=true; get_template_part('archive-header') ;?>
-
 	      <?php while(have_posts()) : the_post() ?>
 		<div class="product <?php get_post_class() ?>">
 		  <div class="product-thumbnail">
+		    <?php
+			$carousel_id = get_post_meta(get_the_ID(), 'image-id', true);
+		    ?>
+		    <a href="<?php the_permalink(); ?><?php echo ($carousel_id ? '#jp-carousel-'.$carousel_id : '') ?>" title="<?php the_title_attribute(); ?>">
 			<?php if ( has_post_thumbnail() ) : ?>
-			    <?php
-				$carousel_id = get_post_meta(get_the_ID(), 'image-id', true);
-    			    ?>
-			    <a href="<?php the_permalink(); ?><?php echo ($carousel_id ? '#jp-carousel-'.$carousel_id : '') ?>" title="<?php the_title_attribute(); ?>">
 				<?php the_post_thumbnail('product-thumbnail'); ?>
-			    </a>
+			<?php else : ?>
+			    <img class´"product-thumbnail" src="<?php bloginfo('template_directory'); ?>/pics/placeholder1-1024x769.png"/>
 			<?php endif; ?>
+		    </a>
 		  </div>
 		  <div class="product-description">
 			  <a href="<?php the_permalink();?>"><h2 class="title"><?php the_title();?></h2></a>
