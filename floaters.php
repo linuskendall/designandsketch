b3123e8ddba00e2e8ac75e615574e9d21af23bdb
Linus Kendall <git@linuskendall.com>
2016-06-21 20:54:40 +0530

    Fixed floaters

--- floaters.php
+++ floaters.php
@@ -1,11 +1,11 @@
 <div class="floaters">
     <div id="contactme">
-      <img src="<?php bloginfo('template_url');?>/pics/contactme.svg">
+      <a href="<?php echo home_url( 'about-me/'); ?>#form"><img src="<?php bloginfo('template_url');?>/pics/contactme.svg"></a>
     </div>
 
       <div id="socialbuttons">
-      <img src="<?php bloginfo('template_url');?>/pics/fb.svg">
-      <img src="<?php bloginfo('template_url');?>/pics/be.svg">
-      <img src="<?php bloginfo('template_url');?>/pics/pin.svg">
+      <a href="https://www.facebook.com/nathrupsa?em=1"/><img src="<?php bloginfo('template_url');?>/pics/fb.svg"></a>
+     <!-- <img src="<?php bloginfo('template_url');?>/pics/be.svg">
+      <img src="<?php bloginfo('template_url');?>/pics/pin.svg">-->
     </div>
 </div>
