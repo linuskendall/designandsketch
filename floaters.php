fc9c0c5d1abe8683e52185c416f0f6b6d8b5ec7e
Linus Kendall <git@linuskendall.com>
2016-06-22 13:05:52 +0530

    Fixed instagram

--- floaters.php
+++ floaters.php
@@ -3,9 +3,8 @@
       <a href="<?php echo home_url( 'about-me/'); ?>#form"><img src="<?php bloginfo('template_url');?>/pics/contactme.svg"></a>
     </div>
 
-      <div id="socialbuttons">
+    <div id="socialbuttons">
       <a href="https://www.facebook.com/nathrupsa?em=1"/><img src="<?php bloginfo('template_url');?>/pics/fb.svg"></a>
-     <!-- <img src="<?php bloginfo('template_url');?>/pics/be.svg">
-      <img src="<?php bloginfo('template_url');?>/pics/pin.svg">-->
+      <a href="https://www.instagram.com/nathrupsa/"/><img src="<?php bloginfo('template_url');?>/pics/instagram-01.png"></a>
     </div>
 </div>
