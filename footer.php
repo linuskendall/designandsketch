c8b5ac667563d1778c6f49ecfb5bf19cedaf4458
Linus Kendall <git@linuskendall.com>
2016-06-22 13:31:30 +0530

    Fixed copyright notice

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<footer style="margin: 0 auto;"><p><small>Copyright &copy; 2016<?php echo (date("Y") > 2016 ?  "-".date("Y") : ""); ?>, Rupsa Nath, All rights reserved</small></p></footer>
 
 </div>
 
